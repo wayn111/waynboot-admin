45e8c2ca5a2daf5be95ba15a237b446accbdf01d
wayn <1669738430@qq.com>
2024-05-05 17:05:36 +0800

    代码全局优化

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/admin',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
